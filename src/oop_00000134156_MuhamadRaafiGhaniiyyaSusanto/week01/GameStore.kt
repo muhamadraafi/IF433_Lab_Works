3df976e08ebaf1fb6081d548bd7f12fb253c9f49
muhamadraafi <mraafigs1923@gmail.com>
2026-02-10 22:25:43 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week01/GameStore.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week01/GameStore.kt
@@ -2,16 +2,19 @@ package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week01
 
 fun calculateDiscount(price: Int) = if (price > 500000) (price * 0.8).toInt() else (price * 0.9).toInt()
 
-fun printReceipt(title: String, finalPrice: Int) {
+fun printReceipt(title: String, finalPrice: Int, userName: String?) {
     println("Judul: $title")
     println("Harga Akhir: $finalPrice")
+    // Elvis Operator sesuai modul
+    println("Catatan User: ${userName ?: "Tidak ada catatan"}")
 }
 
 fun main() {
     val gameTitle = "Elden Ring"
     val price = 750000
     val finalPrice = calculateDiscount(price)
+    val userName: String? = null  // Challenge null safety
 
-    // WAJIB menggunakan Named Arguments
-    printReceipt(title = gameTitle, finalPrice = finalPrice)
+    // Named Arguments
+    printReceipt(title = gameTitle, finalPrice = finalPrice, userName = userName)
 }

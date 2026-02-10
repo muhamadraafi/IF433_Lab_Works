556b7726bba3e1f0c992befcd6ab678e6619e37c
muhamadraafi <mraafigs1923@gmail.com>
2026-02-10 22:25:04 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week01/GameStore.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week01/GameStore.kt
@@ -2,8 +2,16 @@ package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week01
 
 fun calculateDiscount(price: Int) = if (price > 500000) (price * 0.8).toInt() else (price * 0.9).toInt()
 
+fun printReceipt(title: String, finalPrice: Int) {
+    println("Judul: $title")
+    println("Harga Akhir: $finalPrice")
+}
+
 fun main() {
     val gameTitle = "Elden Ring"
     val price = 750000
     val finalPrice = calculateDiscount(price)
+
+    // WAJIB menggunakan Named Arguments
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
 }

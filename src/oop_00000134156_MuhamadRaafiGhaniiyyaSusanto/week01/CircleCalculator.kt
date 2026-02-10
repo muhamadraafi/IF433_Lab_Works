cc923a4bf336f37dd3ed1e6c520753826884e47d
muhamadraafi <mraafigs1923@gmail.com>
2026-02-10 22:18:47 +0700

    week01: (tugas 1) add legacy CircleCalculator code

---
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week01/CircleCalculator.kt
@@ -0,0 +1,11 @@
+package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week01
+
+fun main(args: Array<String>) {
+    // Variable definition
+    var radius: Double = 7.0
+    var pi: Double = 3.14
+    // Calculation
+    var area: Double = pi * radius * radius
+    // Output Concatenation
+    println("Luas lingkaran dengan radius " + radius + " adalah " + area)
+}

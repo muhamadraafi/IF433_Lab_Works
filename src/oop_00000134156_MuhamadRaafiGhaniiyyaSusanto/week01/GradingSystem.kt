dbf885b934b694595c49b58476d9c275154073d2
muhamadraafi <mraafigs1923@gmail.com>
2026-02-10 22:11:02 +0700

    week01: add calculateStatus function

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week01/GradingSystem.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week01/GradingSystem.kt
@@ -1,17 +1,23 @@
 package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week01
 
+// CHECKPOINT 4: Expression Body Function
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
+
 fun main() {
     val name = "John Thor"
     val score = 80
     println("Nama: $name, Nilai: $score")
 
-    // DITAMBAHKIN di sini ↓↓↓
-    val grade = when (score) {
-        in 90..100 -> "A"
-        in 80..89 -> "B"
-        in 70..79 -> "C"
-        in 60..69 -> "D"
+    // CHECKPOINT 3: When Expression
+    val grade = when {
+        score >= 90 -> "A"
+        score >= 80 -> "B"
+        score >= 70 -> "C"
+        score >= 60 -> "D"
         else -> "E"
     }
     println("Grade: $grade")
+
+    // CHECKPOINT 4: Panggil fungsi
+    println("Status: ${calculateStatus(score)}")
 }

19baf7764607febb2d79bc25e1636b0c486ebd29
muhamadraafi <mraafigs1923@gmail.com>
2026-02-10 22:20:42 +0700

    week01: (tugas 1) clean up unused args and comments

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week01/CircleCalculator.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week01/CircleCalculator.kt
@@ -1,11 +1,8 @@
 package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week01
 
-fun main(args: Array<String>) {
-    // Variable definition
+fun main() {
     val radius = 7.0
     val pi = 3.14
-    // Calculation
     val area = pi * radius * radius
-    // Output Concatenation
     println("Luas lingkaran dengan radius $radius adalah $area")
 }

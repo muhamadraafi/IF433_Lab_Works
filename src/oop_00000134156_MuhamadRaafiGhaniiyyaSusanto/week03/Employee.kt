988a4d9bbb2dc57b15078f18e49775f7c8630c38
muhamadraafi <mraafigs1923@gmail.com>
2026-02-24 22:38:53 +0700

    week03: fix setter using backing field

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week03/Employee.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week03/Employee.kt
@@ -3,7 +3,11 @@ package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week03
 class Employee(val name: String) {
     var salary: Int = 0
         set(value) {
-            println("Setter dipanggil dengan nilai: $value")
-            this.salary = value  // INI SALAH! Akan menyebabkan rekursi tak terbatas
+            if (value >= 0) {
+                field = value  // Gunakan 'field' bukan 'this.salary'
+                println("Gaji ${this.name} diubah menjadi $value")
+            } else {
+                println("Gaji tidak boleh negatif!")
+            }
         }
 }
--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week03/Main.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week03/Main.kt
@@ -2,6 +2,12 @@ package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week03
 
 fun main() {
     val e = Employee("Budi")
+
+    println("Mencoba set gaji 5000:")
     e.salary = 5000
-    println("Salary: ${e.salary}")
+
+    println("\nMencoba set gaji -100:")
+    e.salary = -100
+
+    println("\nGaji akhir: ${e.salary}")
 }

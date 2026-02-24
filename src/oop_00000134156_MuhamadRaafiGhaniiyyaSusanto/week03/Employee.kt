36b446f0aed8c02d51db8e1963c062e03d6d93df
muhamadraafi <mraafigs1923@gmail.com>
2026-02-24 22:36:52 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week03/Employee.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week03/Employee.kt
@@ -2,4 +2,8 @@ package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Setter dipanggil dengan nilai: $value")
+            this.salary = value  // INI SALAH! Akan menyebabkan rekursi tak terbatas
+        }
 }
---
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week03
+
+fun main() {
+    val e = Employee("Budi")
+    e.salary = 5000
+    println("Salary: ${e.salary}")
+}

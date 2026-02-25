38f945e11bd6778bb01fef1fed10e758d29e3369
muhamadraafi <mraafigs1923@gmail.com>
2026-02-25 10:30:54 +0700

    week04: (task 2) create Developer class

---
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week04/Developer.kt
@@ -0,0 +1,11 @@
+package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week04
+
+class Developer(name: String, baseSalary: Int, val programmingLanguage: String)
+    : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang ngoding menggunakan $programmingLanguage.")
+    }
+
+    // calculateBonus() tidak di-override, menggunakan bawaan dari Employee
+}

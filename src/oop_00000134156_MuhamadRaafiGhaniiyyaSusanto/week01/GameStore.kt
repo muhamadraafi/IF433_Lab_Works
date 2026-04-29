64d4ef01e510df42f2577935a7deffda437e5cda
muhamadraafi <mraafigs1923@gmail.com>
2026-04-29 19:43:07 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week01/GameStore.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week01/GameStore.kt
@@ -4,11 +4,19 @@ fun main() {
     val gameTitle = "Elden Ring"
     val price = 750000
 
-    val finalPrice = price - calculateDiscount(price)
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
 
-    println("Game: $gameTitle")
-    println("Harga Asli: Rp$price")
-    println("Harga Akhir: Rp$finalPrice")
+    printReceipt(title = gameTitle, originalPrice = price, discount = discount, finalPrice = finalPrice)
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
+
+fun printReceipt(title: String, originalPrice: Int, discount: Int, finalPrice: Int) {
+    println("========== STRUK PEMBELIAN ==========")
+    println("Game: $title")
+    println("Harga Asli: Rp$originalPrice")
+    println("Diskon: Rp$discount")
+    println("Harga Akhir: Rp$finalPrice")
+    println("=====================================")
+}

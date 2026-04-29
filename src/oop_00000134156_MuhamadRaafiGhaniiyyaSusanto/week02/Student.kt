3c53be305d8c27f3aa32632a2ffd437b59584f7f
muhamadraafi <mraafigs1923@gmail.com>
2026-04-29 20:01:45 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week02/Student.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week02/Student.kt
@@ -3,7 +3,8 @@ package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week02
 class Student(
     val name: String,
     val nim: String,
-    var major: String
+    var major: String,
+    var gpa: Double = 0.0
 ) {
     init {
         if (nim.length != 5) {

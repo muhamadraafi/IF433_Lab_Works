473a8e8b808a7269abee53be3ebe2aa5a188bbf8
muhamadraafi <mraafigs1923@gmail.com>
2026-04-29 20:07:59 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week02/Hero.kt
@@ -0,0 +1,23 @@
+package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week02
+
+class Hero(
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100
+) {
+    fun attack(targetName: String) {
+        println("⚔️ $name menebas $targetName! ⚔️")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        if (hp < 0) {
+            hp = 0
+        }
+        println("💔 $name menerima $damage damage! Sisa HP: $hp 💔")
+    }
+
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+}

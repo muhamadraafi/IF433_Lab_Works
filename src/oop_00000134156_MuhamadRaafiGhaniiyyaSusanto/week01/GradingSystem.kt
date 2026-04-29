35bf5e48e03fafd5af17d3e0004be76351682737
muhamadraafi <mraafigs1923@gmail.com>
2026-04-29 19:24:58 +0700

    week01: add calculateStatus function

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week01/GradingSystem.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week01/GradingSystem.kt
@@ -13,4 +13,7 @@ fun main() {
     }
 
     println("Nama: $name, Nilai: $score, Grade: $grade")
+    println("Status: ${calculateStatus(score)}")
 }
+
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"

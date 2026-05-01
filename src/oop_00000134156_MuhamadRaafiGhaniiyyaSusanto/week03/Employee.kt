c824085a5908fc63f76e9d5f77d8e9c30052f2ee
muhamadraafi <mraafigs1923@gmail.com>
2026-05-01 11:11:45 +0700

    week03: fix setter using backing field

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week03/Employee.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week03/Employee.kt
@@ -3,6 +3,11 @@ package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week03
 class Employee(val name: String) {
     var salary: Int = 0
         set(value) {
-            this.salary = value  // INI AKAN CAUSE STACK OVERFLOW!
+            if (value < 0) {
+                println("WARNING: Gaji tidak boleh negatif! Nilai tidak diubah.")
+            } else {
+                field = value  // Menggunakan backing field
+                println("Gaji $name berhasil diupdate menjadi Rp$field")
+            }
         }
 }
--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week03/Main.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week03/Main.kt
@@ -2,6 +2,11 @@ package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week03
 
 fun main() {
     val emp = Employee("Budi")
+    println("Mencoba set gaji 5000:")
     emp.salary = 5000
-    println("Gaji: ${emp.salary}")
+    println("Gaji sekarang: ${emp.salary}")
+
+    println("\nMencoba set gaji -1000 (harus ditolak):")
+    emp.salary = -1000
+    println("Gaji sekarang: ${emp.salary}")
 }

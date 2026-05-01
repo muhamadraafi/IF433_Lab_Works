cd4c5b8753ae0e9c57412ae9af090f5b38baaecd
muhamadraafi <mraafigs1923@gmail.com>
2026-05-01 11:39:21 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week04/Main.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week04/Main.kt
@@ -1,6 +1,8 @@
 package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week04
 
 fun main() {
+    // ===== GUIDED LAB =====
+    println("========== GUIDED LAB ==========")
     println("--- Testing Vehicle ---")
     val generalVehicle = Vehicle(brand = "Sepeda Onthel")
     generalVehicle.honk()
@@ -17,4 +19,17 @@ fun main() {
     myElectricCar.accelerate()
     myElectricCar.honk()
     myElectricCar.openTrunk()
+
+    // ===== TASK 2: HIERARKI KARYAWAN =====
+    println("\n========== TASK 2: HIERARKI KARYAWAN ==========")
+
+    val manager = Manager(name = "Budi", baseSalary = 10000000)
+    println("--- Manager ---")
+    manager.work()
+    println("Bonus Manager: Rp${manager.calculateBonus()}")
+
+    val developer = Developer(name = "Andi", baseSalary = 8000000, programmingLanguage = "Kotlin")
+    println("\n--- Developer ---")
+    developer.work()
+    println("Bonus Developer: Rp${developer.calculateBonus()}")
 }

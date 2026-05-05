f319b5e490465c7a24b374983b033bff066ed5de
muhamadraafi <mraafigs1923@gmail.com>
2026-05-06 05:27:13 +0700

    week09: implement set to discard duplicate elements

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week09/CollectionsBasics.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week09/CollectionsBasics.kt
@@ -3,13 +3,18 @@ package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week09
 fun main() {
     println("=== TEST LIST IMMUTABLE VS MUTABLE ===")
 
-    // Immutable List (Read-Only)
     val readOnlyList = listOf("Apple", "Banana", "Cherry")
     println("Read-Only List: $readOnlyList")
 
-    // Mutable List (Bisa diubah)
     val mutableList = mutableListOf("Apple", "Banana", "Cherry")
     mutableList.add("Durian")
     mutableList.remove("Banana")
     println("Mutable List after changes: $mutableList")
+
+    println("\n=== TEST SET (UNIQUE ELEMENTS) ===")
+    val duplicateList = listOf("BTC", "ETH", "BTC", "SOL", "ETH", "XRP")
+    println("Original list with duplicates: $duplicateList")
+
+    val uniqueSet = duplicateList.toSet()
+    println("Set after removing duplicates: $uniqueSet")
 }

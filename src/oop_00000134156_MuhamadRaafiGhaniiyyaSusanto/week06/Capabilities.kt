fa43742aec363add18faa425aafb17fdd2ce3d7f
muhamadraafi <mraafigs1923@gmail.com>
2026-05-05 21:54:42 +0700

    week06: create compound interfaces

---
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week06
+
+// Interface = Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}

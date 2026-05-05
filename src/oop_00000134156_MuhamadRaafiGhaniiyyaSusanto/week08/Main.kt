0478b16a2d2fe00592ce492531591b7fbf021dea
muhamadraafi <mraafigs1923@gmail.com>
2026-05-05 23:44:29 +0700

    week08: implement java interop using !!

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week08/Main.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week08/Main.kt
@@ -44,11 +44,16 @@ fun main() {
 
     val apiResponse: Map<String, String?> = mapOf("status" to "200", "token" to null)
     try {
-        // Alih-alih apiResponse["token"]!!
         val token = requireNotNull(apiResponse["token"]) {
             "CRITICAL EXCEPTION: Token otentikasi tidak ditemukan dari server!"
         }
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
+
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+    // Kita menekan tombol !! karena KITA TAHU implementasi Java-nya aman
+    val statusLength = javaResponse!!.length
+    println("Status dari Java: $javaResponse (Length: $statusLength)")
 }

4772542afe8ce5fc732f18c9fe98d9df09986d83
muhamadraafi <mraafigs1923@gmail.com>
2026-05-06 06:15:43 +0700

    week10: (task) initialize coin repository

---
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week10/CryptoDashboard.kt
@@ -0,0 +1,8 @@
+package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week10
+
+fun main() {
+    println("=== CRYPTO WALLET DASHBOARD ===")
+
+    val coinRepo = WalletRepository<Coin>()
+    println("Coin repository initialized!")
+}

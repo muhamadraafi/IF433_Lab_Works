3ea1ce23d7e35b2b6ae15b8727a5dfd77cd529a1
muhamadraafi <mraafigs1923@gmail.com>
2026-05-06 06:16:35 +0700

    week10: (task) populate coin repository

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week10/CryptoDashboard.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week10/CryptoDashboard.kt
@@ -4,5 +4,11 @@ fun main() {
     println("=== CRYPTO WALLET DASHBOARD ===")
 
     val coinRepo = WalletRepository<Coin>()
-    println("Coin repository initialized!")
+
+    // Menambah minimal 3 koin
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 4.2))
+    coinRepo.add(Coin("USDT", 1000.0))
+
+    println("Coins added to repository!")
 }

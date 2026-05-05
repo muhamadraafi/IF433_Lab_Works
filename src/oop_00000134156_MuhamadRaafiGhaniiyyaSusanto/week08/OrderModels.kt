43a4e72969651494603f3c7ea388f4a1100b5e73
muhamadraafi <mraafigs1923@gmail.com>
2026-05-05 23:21:51 +0700

    week08: create nested classes for Order

---
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week08/OrderModels.kt
@@ -0,0 +1,9 @@
+package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week08
+
+class City(val name: String)
+
+class Address(val city: City?)
+
+class DeliveryDetails(val address: Address?)
+
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)

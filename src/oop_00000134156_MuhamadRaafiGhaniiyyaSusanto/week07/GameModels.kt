0c6b151971885798c0f05cc68570d33843e2fb55
muhamadraafi <mraafigs1923@gmail.com>
2026-05-05 23:02:31 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1)
+}

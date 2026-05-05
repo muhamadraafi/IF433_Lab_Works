3096fc24ffe64e4cc9eff00d2b36ccd676f9e87b
muhamadraafi <mraafigs1923@gmail.com>
2026-05-05 23:25:44 +0700

    week08: create mixed data collection

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week08/Main.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week08/Main.kt
@@ -9,9 +9,17 @@ fun main() {
     println("\n=== TEST LET BLOCK ===")
     val validOrder = Order(null, 250000)
     val receipt = validOrder.totalPrice?.let { price ->
-        // Blok ini HANYA jalan jika totalPrice tidak null
         val tax = price * 0.11
         "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
     } ?: "Transaksi Invalid: Harga belum di-set!"
     println(receipt)
+
+    println("\n=== TEST SAFE CASTING ===")
+    val mixedData: List<Any> = listOf(
+        "Smartphone",
+        1500000,
+        UserProfile("Andi", null),
+        "Laptop",
+        4500000.0
+    )
 }

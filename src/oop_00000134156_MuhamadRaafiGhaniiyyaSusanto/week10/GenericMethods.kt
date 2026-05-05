e315e1da0c34948c8a50b060a0940840d075da3f
muhamadraafi <mraafigs1923@gmail.com>
2026-05-06 06:05:36 +0700

    week10: implement identity function processData

--- src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week10/GenericMethods.kt
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week10/GenericMethods.kt
@@ -3,3 +3,7 @@ package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week10
 fun <T> printData(data: T) {
     println("Data yang diterima: $data")
 }
+
+fun <T> processData(input: T): T {
+    return input
+}

c3ebf806d347bfb82b5973e937f9b6fe4c133f12
muhamadraafi <mraafigs1923@gmail.com>
2026-05-06 10:56:39 +0700

    week11: (task) initialize smart home device list

---
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week11/SmartHomeDashboard.kt
@@ -0,0 +1,8 @@
+package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week11
+
+fun main() {
+    println("=== SMART HOME CONFIGURATION PIPELINE ===")
+
+    val homeDevices = mutableListOf<SmartDevice>()
+    println("Device list initialized!")
+}

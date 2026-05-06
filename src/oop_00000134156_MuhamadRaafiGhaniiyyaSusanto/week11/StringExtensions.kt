3d779ea13cd8bb829f0a2a605ee93abd439ed044
muhamadraafi <mraafigs1923@gmail.com>
2026-05-06 10:32:44 +0700

    week11: create string extension functions

---
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this!"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}

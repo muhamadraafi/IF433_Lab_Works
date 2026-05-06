650352299933e3066fec2714f3bd370c83320495
muhamadraafi <mraafigs1923@gmail.com>
2026-05-06 10:54:11 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000134156_MuhamadRaafiGhaniiyyaSusanto/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000134156_MuhamadRaafiGhaniiyyaSusanto.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
